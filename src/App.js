fd1978f2d1746e53a859e57cb37a6d225cc71f81
SirPm <odumosuoluwapm@gmail.com>
2021-01-14 12:56:02 +0100

    fixed react-router-dom issue caused by uninstalling the package

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import TableOverview from './component/table-overview/TableOverview';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path='/' component={TableOverview} />
-        </Switch>
-      </BrowserRouter>
+      <TableOverview />
     </div>
   );
 }
